4135c26af1da9c344d08e0add46041a33f9809b6
huzaifa9999 <mhuzaifa.0228@gmail.com>
2023-07-21 14:04:33 +0530

    aaa

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ app.use("/confession", confessionRoutes);
 // HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
 
