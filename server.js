1b2ae04bfb16994d2114ffb2aa6d8d553a53d243
huzaifa9999 <mhuzaifa.0228@gmail.com>
2023-07-21 14:02:11 +0530

    aa

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const server = http.createServer(app);
 // const io = socketIO(server);
 app.use(express.json());
 // app.use(cors({ origin: /http:\/\/(127(\.\d){3}|localhost)/}));
-app.use(express.static(path.join(__dirname, './frontend/build')));
+app.use(express.static(path.join(__dirname, './frontend/')));
 // app.use(express.static(path.join(__dirname, './frontend/public')));
 app.use(cors());
 const io = new Server(server, {
