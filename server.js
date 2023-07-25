03ccb9a8cc43950b67b69ecee1df54121471f17a
huzaifa9999 <mhuzaifa.0228@gmail.com>
2023-07-25 18:57:34 +0530

    deployement 1st

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./database/db");
 const http = require('http');
-const {Server} = require('socket.io');
+const { Server } = require('socket.io');
 const userRoutes = require("./routes/userRoutes");
-const confessionRoutes= require("./routes/confessionRoutes")
+const confessionRoutes = require("./routes/confessionRoutes")
 const cors = require("cors");
 const path = require("path");
 dotenv.config();
 connectDB();
-const port=process.env.port||8080;
+const port = process.env.port || 8080;
 const app = express();
 const server = http.createServer(app);
 // const io = socketIO(server);
@@ -18,10 +18,10 @@ app.use(express.json());
 
 app.use(cors());
 // app.options('*', cors());
-const io= new Server(server,{
-  cors:{
+const io = new Server(server, {
+  cors: {
     methods: ['GET', 'POST'],
-    origin:"*"
+    origin: "*"
   }
 
 })
@@ -46,14 +46,14 @@ io.on('connection', (socket) => {
 // });
 app.use("/user", userRoutes);
 
-app.use("/confession",confessionRoutes)
+app.use("/confession", confessionRoutes)
 
 //production script
 
-const __dirname1=path.resolve();
-app.use(express.static(path.join(__dirname1,"./frontend/build")))
-app.get("*",(req,res)=>{
-  res.sendFile(path.resolve(__dirname1,"frontend","build","index.html"));
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "./frontend/build")))
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
 })
 
 server.listen(port, () => {
